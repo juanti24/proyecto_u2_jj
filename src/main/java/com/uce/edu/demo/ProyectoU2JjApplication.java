e50291ba509dea6a8a1ccdacd5d4a7f68bbac1c4
Juan Jumbo <tiniyan24@gmail.com>
2022-06-24 20:02:40 -0500

    Taller Catorce: Configuracion base de datos

--- src/main/java/com/uce/edu/demo/ProyectoU2JjApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2JjApplication.java
@@ -1,13 +1,20 @@
 package com.uce.edu.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProyectoU2JjApplication {
+public class ProyectoU2JjApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2JjApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hola Mundo");
+		
 	}
 
 }
